e97a6ebe1d1b21aded3559cbe6d0385363ace40b
James Home <james@jameshome.co.uk>
2021-06-05 23:08:07 +0100

    Add sm

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,55 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'jameshomecouk',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    meta: [{
+      charset: 'utf-8'
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    }, {
+      hid: 'description',
+      name: 'description',
+      content: ''
+    }],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }],
+    script: [{
+      src: "https://cdn.polyfill.io/v2/polyfill.min.js?features=Element.prototype.classList"
+    }, {
+      src: "https://cdn.jsdelivr.net/npm/focus-visible@5.0.2/dist/focus-visible.min.js"
+    }]
   },
-
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
-
+  css: ["vue-essential-slices/src/styles/styles.scss"],
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
-
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
+  buildModules: [// https://go.nuxtjs.dev/eslint
+  '@nuxtjs/eslint-module', // https://go.nuxtjs.dev/tailwindcss
+  '@nuxtjs/tailwindcss'],
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
+  modules: [// https://go.nuxtjs.dev/axios
+  '@nuxtjs/axios', ["@nuxtjs/prismic", {
+    endpoint: "https://jameshome.cdn.prismic.io/api/v2",
+    apiOptions: {
+      routes: [{
+        type: "page",
+        path: "/:uid"
+      }]
+    }
+  }]],
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
-
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
-}
+  build: {
+    transpile: ["vue-slicezone", "nuxt-sm"]
+  }
+};
